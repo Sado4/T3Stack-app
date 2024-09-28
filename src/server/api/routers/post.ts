993bfb55c76d2fa0f69182f2f88ff88466a7f791
Sado4 <derasado04@gmail.com>
2024-09-28 19:39:21 +0900

    feat: tRPC practice

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -5,6 +5,7 @@ import {
   protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
+import { db } from "~/server/db";
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
@@ -15,24 +16,56 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
-  create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      return ctx.db.post.create({
+  getAllBlogs: publicProcedure.query(() => {
+    return db.post.findMany();
+  }),
+
+  postBlog: publicProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        description: z.string(),
+      }),
+    )
+    .mutation(({ input }) => {
+      return db.post.create({
         data: {
-          name: input.name,
-          createdBy: { connect: { id: ctx.session.user.id } },
+          title: input.title,
+          description: input.description,
         },
       });
     }),
 
-  getLatest: protectedProcedure.query(async ({ ctx }) => {
-    const post = await ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
-      where: { createdBy: { id: ctx.session.user.id } },
-    });
+  // 詳細ページの取得
+  getDetailBlog: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .query(({ input }) => {
+      return db.post.findUnique({
+        where: { id: input.id },
+      });
+    }),
 
-    return post ?? null;
+  // create: protectedProcedure
+  //   .input(z.object({ name: z.string().min(1) }))
+  //   .mutation(async ({ ctx, input }) => {
+  //     return ctx.db.post.create({
+  //       data: {
+  //         name: input.name,
+  //         createdBy: { connect: { id: ctx.session.user.id } },
+  //       },
+  //     });
+  //   }),
+
+  getLatest: publicProcedure.query(async ({ ctx }) => {
+    // const post = await ctx.db.post.findFirst({
+    //   orderBy: { createdAt: "desc" },
+    //   where: { createdBy: { id: ctx.session.user.id } },
+    // });
+
+    // return post ?? null;
+
+    // FIXME: NextAuthを使わないため、一旦nullを返す
+    return null;
   }),
 
   getSecretMessage: protectedProcedure.query(() => {
